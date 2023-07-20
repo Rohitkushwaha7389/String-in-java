d26f108f67482fcf0cf6e78c3fb0cc84dfef7ea8
Rohit Kushwaha <113577083+Rohitkushwaha7389@users.noreply.github.com>
2023-07-20 22:38:38 +0530

    Create  palindrome.java

---
+++ import java.util.Scanner;  public class palindrome {     public static void main(String[]args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int r,sum=0,temp;         temp=n;         while(n>0){             r=n%10;             sum=(sum*10)+r;             n=n/palindrome.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public classimport java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r,sum=0,temp;
+        temp=n;
+        while(n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+        System.out.println("palindrome");
+        else 
+     System.out.println("not palindrome"  );
+    }
+    
+}
+ {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r,sum=0,temp;
+        temp=n;
+        while(n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+        System.out.println("palindrome");
+        else 
+     System.out.println("not palindrome"  );
+    }
+    
+}
